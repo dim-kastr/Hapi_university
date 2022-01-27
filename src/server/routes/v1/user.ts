023353f9913f0810d14ee976c8a404cb79905d7b
dim-kastr <swagmyfamily@icloud.com>
2022-01-27 16:49:27 +0700

    task1.0

---
+++ src/server/routes/v1/user.ts
@@ -0,0 +1,28 @@
+import * as users from '../../api/v1/user';
+import * as valid from '../../schemes/index';
+
+
+export default [
+    {
+        method: 'POST',
+        path: '/v1/user/authentication',
+        handler: users.userAuthentication,
+        options: {
+            validate: {
+                payload: valid.userValid
+            },
+            auth: false
+        }
+    },
+    {
+        method: 'POST',
+        path: '/v1/user/registration',
+        handler: users.userRegistration,
+        options: {
+            validate: {
+                payload: valid.userValid
+            },
+            auth: false
+        }
+    },
+];
--- src/server/schemes/index.ts
+++ src/server/schemes/index.ts
@@ -4,3 +4,15 @@ export const outputOkSchema = (res: Joi.Schema): Joi.Schema => Joi.object({
   ok: Joi.boolean().example(true),
   result: res,
 });
+
+const userValid =
+  Joi.object({
+    username: Joi.string().alphanum().min(6).max(8).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/).required(),
+    phone: Joi.string().pattern(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/).required(),
+    dateOfBirth: Joi.date().raw(),
+    sex: Joi.string().valid('male', 'female').required(),
+  })
+
+export { userValid };
