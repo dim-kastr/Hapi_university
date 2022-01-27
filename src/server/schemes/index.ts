9020ad0d48522eece13ba099eac939838dbaa6a1
dim-kastr <swagmyfamily@icloud.com>
2022-01-27 18:26:05 +0700

    task1.1

--- src/server/schemes/index.ts
+++ src/server/schemes/index.ts
@@ -5,14 +5,20 @@ export const outputOkSchema = (res: Joi.Schema): Joi.Schema => Joi.object({
   result: res,
 });
 
-const userValid =
+const userValidRegistr =
   Joi.object({
     username: Joi.string().alphanum().min(6).max(8).required(),
     email: Joi.string().email().required(),
     password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/).required(),
     phone: Joi.string().pattern(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/).required(),
-    dateOfBirth: Joi.date().raw(),
+    dateOfBirth: Joi.date().raw().required(),
     sex: Joi.string().valid('male', 'female').required(),
   })
 
-export { userValid };
+const userValidAuth =
+  Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/).required(),
+  })
+
+export { userValidRegistr, userValidAuth };
